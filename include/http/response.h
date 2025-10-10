75e0a8c12e006efd417a840fd370637790183c5f
Luis Angel Silva Mondragon <luisangel@Victors-MacBook-Air.local>
2025-10-10 17:22:46 -0600

    Response code
    Response interface defined
    Status codes defined

--- include/http/response.h
+++ include/http/response.h
@@ -1,9 +1,23 @@
 #ifndef RESPONSE_H
 #define RESPONSE_H
 
+#include <stdint.h>
 struct response;
-typedef struct response response;
+typedef struct http_response http_response;
 
+http_response*  http_response_new();
 
+char*           http_response_bytes(http_response* this);
+
+void            http_response_delete(http_response* this);
+
+uint16_t        http_response_StatusCode(http_response* this);
+const char*     http_response_ReasonPhrase(http_response* this);
+const char*     http_response_Version(http_response* this);
+int             http_response_HeaderSetValue(http_response* this, const char* headerKey, const char* headerValue);
+const char*     http_response_HeaderGetValue(http_response* this, const char* headerKey);
+
+const char**    http_response_HeaderKeys(http_response* this, const char* headerKey);
+bool            http_response_HeaderContains(http_response* this, const char* headerKey);
 
 #endif
--- src/response/response.c
+++ src/response/response.c
@@ -1,3 +1,23 @@
 #include "response_internal.h"
 #include <http/response.h>
 
+#include "response/response_codes.h"
+#include "sds.h"
+#include <stdlib.h>
+
+http_response*   http_response_new() {
+    http_response* new_response = malloc(sizeof(http_response));
+
+    new_response->status_code = HTTP_NO_STATUS;
+    new_response->reason_phrase = sdsnew("");
+    new_response->version = "HTTP/1.1";
+    new_response->header = NULL;
+    new_response->body.length = 0;
+    new_response->body.data = NULL;
+
+    return new_response;
+}
+
+char*       http_response_bytes(http_response* this);
+
+void        http_response_delete(http_response* this);
