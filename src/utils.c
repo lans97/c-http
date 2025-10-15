a276ff88e318c5289e23f13db6fe55fe7eafff95
Luis Angel Silva Mondragon <luisangel@Victors-MacBook-Air.local>
2025-10-15 17:33:47 -0600

    some more things missing

--- src/utils.c
+++ src/utils.c
@@ -1,16 +1,4 @@
 #include "http/utils.h"
-#include "http/version.h"
-
-bool http_version_isValid(http_version *this) {
-    if (this->major == 0 && this->minor == 9) return true;
-    if (this->major == 1) {
-        if (this->minor == 0) return true;
-        if (this->minor == 1) return true;
-    }
-    if (this->major == 2 && this->minor == 0) return true;
-    if (this->major == 3 && this->minor == 0) return true;
-    return false;
-}
 
 void free_sdsarr(sds *arr, int arrlen) {
     for (int i = 0; i < arrlen; i++) {
