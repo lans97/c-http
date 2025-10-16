8e5dbf3b610d421dee0e5499b73bba90f53a7f5d
Luis Angel Silva Mondragon <luisangel@Victors-MacBook-Air.local>
2025-10-16 17:18:28 -0600

    connection refinmenet

--- src/server.c
+++ src/server.c
@@ -1,3 +1,5 @@
+#include "http/request.h"
+#include "http/results.h"
 #include "logger/logger.h"
 #include "sds.h"
 #include "server_internal.h"
@@ -20,15 +22,14 @@ http_connection *http_connection_new() {
     return new_connection;
 }
 
-int http_connection_bindAndListen(http_connection *this, int port) {
+ErrorMessage http_connection_bindAndListen(http_connection *this, int port) {
     int client_fd;
     struct sockaddr_in address;
     socklen_t addrlen = sizeof(address);
 
     this->fd = socket(AF_INET, SOCK_STREAM, 0);
     if (this->fd == -1) {
-        LOG_ERROR("Connection error: could not create socket: %s", strerror(errno));
-        return 1;
+        return "Connection error: could not create socket";
     }
 
     address.sin_family = AF_INET;
@@ -36,19 +37,16 @@ int http_connection_bindAndListen(http_connection *this, int port) {
     address.sin_port = htons(port);
 
     if(bind(this->fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-        LOG_ERROR("Connection error: could not bind socket: %s", strerror(errno));
         close(this->fd);
-        return 1;
+        return "Connection error: could not bind socket";
     }
 
     if (listen(this->fd, 10) < 0) {
-        LOG_ERROR("Connection error: could not listen through socket: %s", strerror(errno));
         close(this->fd);
-        return 1;
+        return "Connection error: could not listen through socket";
     }
 
     LOG_DEBUG("Server listening on port %d...\n", port);
-
 
     char tmp[READ_CHUNK];
     while (true) {
@@ -58,11 +56,45 @@ int http_connection_bindAndListen(http_connection *this, int port) {
             continue;
         }
 
-        // TODO: Manage heap buffer cleaning
         size_t nread;
-        while((nread = read(client_fd, tmp, sizeof(tmp))) > 0)
+        while((nread = read(client_fd, tmp, sizeof(tmp))) > 0) {
             this->buffer = sdscatlen(this->buffer, tmp, nread);
+            // TODO: Look for complete request or keep_alive: false
+            // probably change parse strategy to stream reading strategy
+        }
 
+        if (nread < 0) {
+            LOG_ERROR("Error reading from client: %s", strerror(errno));
+            sdsclear(this->buffer);
+            close(client_fd);
+            continue;
+        }
+
+        // PROCESS buffer
+        HTTPRequestResult req_res = http_request_new();
+        if (!req_res.Ok) {
+            LOG_ERROR("Error allocating request object: %s", req_res.Err);
+            sdsclear(this->buffer);
+            close(client_fd);
+            continue;
+        }
+
+        http_request* req = req_res.Value;
+        ErrorMessage reqErr = http_request_parse(req, this->buffer, sdslen(this->buffer));
+        if (reqErr){
+            LOG_ERROR("Error parsing request: %s", reqErr);
+            http_request_delete(req);
+            sdsclear(this->buffer);// conn object conn.buffer gets reused
+            close(client_fd);
+            continue;
+        }
+
+        // TODO: Read METHOD and pass to funcition in TRIE (TRIE not implemented yet)
+
+        // CLEAN
+        sdsclear(this->buffer);
+        http_request_delete(req);
+        close(client_fd);
     }
 }
 
